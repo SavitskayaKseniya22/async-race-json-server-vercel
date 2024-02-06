e94d03f19d5cf7c309e05cff1f41ca7f4576a61a
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-02-06 23:47:31 +0200

    feat: add server script

--- api/server.js
+++ api/server.js
@@ -1,31 +1,99 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
+const PORT = 3000;
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const state = { velocity: {}, blocked: {} };
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+server.use(middlewares);
 
-const middlewares = jsonServer.defaults()
+server.patch('/engine', (req, res) => {
+  const { id, status } = req.query;
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
+    return res
+      .status(400)
+      .send(
+        'Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"'
+      );
+  }
 
-// Export the Server API
-module.exports = server
+  if (!db.garage.find((car) => car.id === +id)) {
+    return res
+      .status(404)
+      .send('Car with such id was not found in the garage.');
+  }
+
+  const distance = 500000;
+  if (status === 'drive') {
+    const velocity = state.velocity[id];
+
+    if (!velocity)
+      return res
+        .status(404)
+        .send(
+          'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?'
+        );
+    if (state.blocked[id])
+      return res
+        .status(429)
+        .send(
+          "Drive already in progress. You can't run drive for the same car twice while it's not stopped."
+        );
+
+    state.blocked[id] = true;
+
+    const x = Math.round(distance / velocity);
+
+    if (new Date().getMilliseconds() % 3 === 0) {
+      setTimeout(() => {
+        delete state.velocity[id];
+        delete state.blocked[id];
+        res
+          .header('Content-Type', 'application/json')
+          .status(500)
+          .send("Car has been stopped suddenly. It's engine was broken down.");
+      }, (Math.random() * x) ^ 0);
+    } else {
+      setTimeout(() => {
+        delete state.velocity[id];
+        delete state.blocked[id];
+        res
+          .header('Content-Type', 'application/json')
+          .status(200)
+          .send(JSON.stringify({ success: true }));
+      }, x);
+    }
+  } else {
+    const x = req.query.speed ? +req.query.speed : (Math.random() * 2000) ^ 0;
+
+    const velocity =
+      status === 'started' ? Math.max(50, (Math.random() * 200) ^ 0) : 0;
+
+    if (velocity) {
+      state.velocity[id] = velocity;
+    } else {
+      delete state.velocity[id];
+      delete state.blocked[id];
+    }
+
+    setTimeout(
+      () =>
+        res
+          .header('Content-Type', 'application/json')
+          .status(200)
+          .send(JSON.stringify({ velocity, distance })),
+      x
+    );
+  }
+});
+
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
+
+module.exports = server;
